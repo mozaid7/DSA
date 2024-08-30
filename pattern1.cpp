6baaa392270a2e9464c75daa1cf956dea43c001c
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-30 15:25:39 +0530

    stars pattern questions solved

--- pattern1.cpp
+++ pattern1.cpp
@@ -261,9 +261,9 @@ void print21(int n) {
     for (int i=0; i<2*n-1; i++) {
         for (int j=0; j<2*n-1; j++) {
             int top = i;
-            int bottom = j;
+            int bottom = (2*n-2) - i;
             int right = (2*n-2) - j;
-            int left = (2*n-2) - i;
+            int left = j;
             cout << (n - min(min(top,bottom), min(left,right)));
         }
         cout << endl;
