a70b313d0a80d856ab68ad4372449f38cd0fc785
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-30 15:19:31 +0530

    star pattern questions solved

--- pattern1.cpp
+++ pattern1.cpp
@@ -223,7 +223,54 @@ void print18(int n) {
     }    
 }
 
+void print19(int n) {
+    int spaces = 2*n - 2;
+    for (int i=1; i<=2*n-1; i++) {
+        int stars =i;
+        if(i>n) stars = 2*n-i;
+        for(int j=1; j<=stars; j++) {
+            cout << "*";
+        }
+
+        for(int j=1; j<=spaces; j++) {
+            cout << " ";
+        }
+
+        for(int j=1; j<=stars; j++) {
+            cout << "*";
+        }
+        cout << endl;
+        if(i<n) spaces -= 2;
+        else spaces += 2;
+    }
+}
+
+void print20(int n) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(i == 0 || j == 0 || i == n-1 || j == n-1) {
+                cout << "*";
+            }
+            else cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print21(int n) {
+    for (int i=0; i<2*n-1; i++) {
+        for (int j=0; j<2*n-1; j++) {
+            int top = i;
+            int bottom = j;
+            int right = (2*n-2) - j;
+            int left = (2*n-2) - i;
+            cout << (n - min(min(top,bottom), min(left,right)));
+        }
+        cout << endl;
+    }
+}
+
 int main() {
-    int n =5;
-    print18(n);
+    int n =3;
+    print21(n);
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,10 @@
+(left,right)))<<" ";
+        }
+        cout<<endl;
+     }
+}
 
+int main() {
+    int n =3;
+    print21(n);
 }
