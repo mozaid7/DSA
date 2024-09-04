905a098ff8a40491920a7c95aa1ba38ebca7d86d
Mohammed Zaid <mohammedzee98@gmail.com>
2024-09-04 19:38:10 +0530

    merge sort learned

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -34,9 +34,13 @@ void merge(vector<int> &arr, int low, int mid, int high) {
 }
 
 void mergeSort(vector<int> &arr, int low, int high) {
-    if (low>= high) return;
+    if (low >= high) return;
     int mid = (low + high) / 2;
     mergeSort(arr, low, mid);
     mergeSort(arr, mid + 1, high);
     merge(arr, low, mid, high);
+}
+
+void mergeS(vector<int> &arr, int n) {
+    mergeSort(arr, 0, n - 1);
 }
