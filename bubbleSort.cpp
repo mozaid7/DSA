f70b626424d1f3855b93f513e21807b9f9644d7f
Mohammed Zaid <mohammedzee98@gmail.com>
2024-09-04 20:01:31 +0530

    recursive bubble sort added

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -13,6 +13,22 @@ void bubble_sort(int arr[], int n) {
     }
 }
 
+// Bubble Sort by Recursion
+void bubble_sort(int arr[], int n) {
+    // Base Case: range == 1.
+    if (n == 1) return;
+
+    for (int j = 0; j <= n - 2; j++) {
+        if (arr[j] > arr[j + 1]) {
+            int temp = arr[j + 1];
+            arr[j + 1] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    //Range reduced after recursion:
+    bubble_sort(arr, n - 1);
+}
+
 int main() {
     int n;
     cin >> n;
