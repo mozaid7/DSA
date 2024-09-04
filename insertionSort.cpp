db337b917da0e51adf125d33c58b148680a11c6d
Mohammed Zaid <mohammedzee98@gmail.com>
2024-09-04 20:09:22 +0530

    Insertion sort with recursion

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -13,6 +13,21 @@ void insertion_sort(int arr[], int n) {
     }
 }
 
+// Recursive Insertion Sort
+void insertion_sort(int arr[], int i, int n) {
+    // Base Case: i == n.
+    if (i == n) return;
+
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+        int temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+    insertion_sort(arr, i + 1, n);
+}
+
 int main() {
     int n;
     cin >> n;
