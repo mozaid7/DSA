9407ae5ed42bde53bd78bc92096a9c82277027dc
Mohammed Zaid <mohammedzee98@gmail.com>
2024-09-09 17:58:22 +0530

    longest subarray sum ques. completed.

--- arrays.cpp
+++ arrays.cpp
@@ -227,4 +227,44 @@ int singleElement(vector<int> &arr) {
     return xorr;
 }
 
-// Longest Sub-Array with sum K
+// Longest Sub-Array with sum K (for Positives & Zeroes)
+int longestSubarray(vector<int> a, long long k) {
+    int left = 0; int right = 0;
+    long long sum = a[0];
+    int maxLen = 0;
+    int n = a.size();
+    while(right < n) {
+        while(left <= right && sum > k) {
+            sum -= a[left];
+            left++;
+        }
+        if(sum == k) {
+            maxLen = max(maxLen, right - left + 1);
+        }
+        right++;
+        if(right < n) sum += a[right];
+    }
+    return maxLen;
+}
+
+// Longest Sub-Array with sum K (for Negatives & Zeroes)
+int longestSubarray(vector<int> a, int k) {
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for(int i=0; i<a.size(); i++) {
+        sum += a[i];
+        if(sum == k) {
+            maxLen = max(maxLen, i+1);
+        }
+        long long rem = sum - k;
+        if(preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+        if(preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+    return maxLen;
+}
