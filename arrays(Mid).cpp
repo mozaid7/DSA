cab2debb8e193c6b3adc966016136e21127cdc87
Mohammed Zaid <mohammedzee98@gmail.com>
2024-09-10 15:59:52 +0530

    rearrange

--- arrays(Mid).cpp
+++ arrays(Mid).cpp
@@ -36,7 +36,7 @@ void sortArray(vector<int>& arr, int n) {
     }
 }
 
-// Majority element > n/2 of a given array
+// Majority element > n/2 of arr given array
 int majorityElement(vector<int> v) {
     int cnt = 0;
     int el;
@@ -92,4 +92,59 @@ int maximumProfit(vector<int> &prices) {
         mini = min(mini, prices[i]);
     }
     return maxProfit;
+}
+
+// Rearrange Array elements by Sign
+vector<int> rearrangeArray(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> ans(n,0);
+    int posIndex = 0, negIndex = 1;
+    for(int i=0; i<n; i++) {
+        if(nums[i] < 0) {
+            ans[negIndex] = nums[i];
+            negIndex += 2;
+        }
+        else {
+            ans[posIndex] = nums[i];
+            posIndex += 2;
+        }
+    }
+    return ans;
+}
+
+// Rearrange Array elements by Sign where Positive & Negative elements are not the same.
+vector<int> alternateNumbers(vector<int>& arr) {
+    vector<int> pos, neg;
+    int n = arr.size();
+    for(int i=0; i<n; i++) {
+        if(arr[i] > 0) {
+            pos.push_back(arr[i]);
+        }
+        else {
+            neg.push_back(arr[i]);
+        }
+    }
+
+    if(pos.size() > neg.size()) {
+        for(int i=0; i<neg.size(); i++) {
+            arr[2*i] = pos[i];
+            arr[2*i+1] = neg[i];
+        }
+        int index = neg.size()*2;
+        for(int i=neg.size(); i<pos.size(); i++) {
+            arr[index] = pos[i];
+            index++;
+        }
+    }
+    else {
+        for(int i=0; i<pos.size(); i++) {
+            arr[2*i] = pos[i];
+            arr[2*i+1] = neg[i];
+        }
+        int index = pos.size()*2;
+        for(int i=pos.size(); i<neg.size(); i++) {
+            arr[index] = neg[i];
+            index++;
+        }
+    }
 }
