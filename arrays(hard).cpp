dfd188b0dfc76542b735be8f1ade7702927296f7
Mohammed Zaid <mohammedzee98@gmail.com>
2024-09-16 20:23:13 +0530

    hard 2

--- arrays(hard).cpp
+++ arrays(hard).cpp
@@ -65,4 +65,32 @@ vector<int> majorityElement(vector<int> v) {
     return ls;
 }
 
-
+// 3-Sum Problem i.e. nums[i] + nums[j] + nums[k] = 0;
+vector<vector<int>> threeSum(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    for(int i=0; i<n; i++) {
+        if( i>0 && nums[i] == nums[i-1]) continue;
+        int j = i+1;
+        int k = n-1;
+        while(j < k) {
+            int sum = nums[i] + nums[j] + nums[k];
+            if(sum < 0) {
+                j++;
+            }
+            else if (sum > 0){
+                k--;
+            }
+            else {
+                vector<int> temp = { nums[i], nums[j], nums[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while( j < k && nums[j] == nums[j-1]) j++;
+                while( j < k && nums[k] == nums[k+1]) k--;
+            }
+        }
+    }
+    return ans;
+}
