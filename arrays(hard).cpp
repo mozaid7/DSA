10cf32c5cad8e7f5ab3d24010f2d573f5b33b558
Mohammed Zaid <mohammedzee98@gmail.com>
2024-10-13 15:54:20 +0530

    2 completed

--- arrays(hard).cpp
+++ arrays(hard).cpp
@@ -195,3 +195,94 @@ int subarraysWithXorK(vector<int> a, int k) {
     }
     return cnt;
 }
+
+// Merge two sorted arrays without any extra spaces
+void merge(long long arr1[], long long arr2[], int n, int m) {
+    int left = n-1;
+    int right = 0;
+    while(left >= 0 && right < 0) {
+        if(arr1[left] > arr2[right]) {
+            swap(arr1[left], arr2[right]);
+            left--, right++;
+        }
+        else {
+            break;
+        }
+    }
+    sort(arr1, arr1 + n);
+    sort(arr2, arr2 + m);
+}
+
+// Option 2 - Leetcode accepted
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int left = m - 1; // Start from the last valid element in nums1
+    int right = n - 1; // Start from the last element in nums2
+    int k = m + n - 1; // Start from the end of nums1 (combined size)
+
+    // Merge from the back to the front
+    while (left >= 0 && right >= 0) {
+        if (nums1[left] > nums2[right]) {
+            nums1[k--] = nums1[left--];
+        } else {
+            nums1[k--] = nums2[right--];
+        }
+    }
+
+    // If there are remaining elements in nums2, copy them over
+    while (right >= 0) {
+        nums1[k--] = nums2[right--];
+    }
+}
+
+// Count Inversions
+int cnt = 0;
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            cnt += (mid - left + 1);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high) {
+    if (low >= high) return;
+    int mid = (low + high) / 2 ;
+    mergeSort(arr, low, mid);  // left half
+    mergeSort(arr, mid + 1, high); // right half
+    merge(arr, low, mid, high);  // merging sorted halves
+}
+
+int numberOfInversions(vector<int>&a, int n) {
+    mergeSort(a, 0, n - 1);
+    return cnt;
+}
