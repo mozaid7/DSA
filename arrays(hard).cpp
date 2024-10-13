7600010b88315c5e76cac043658793777107fd5e
Mohammed Zaid <mohammedzee98@gmail.com>
2024-10-13 17:21:56 +0530

    hard alomost completed

--- arrays(hard).cpp
+++ arrays(hard).cpp
@@ -286,3 +286,19 @@ int numberOfInversions(vector<int>&a, int n) {
     mergeSort(a, 0, n - 1);
     return cnt;
 }
+
+// Maximum product sub-array
+int subArrayWithMaxProduct(vector<int> &arr) {
+    int prefix = 1, suffix = 1;
+    int ans = INT_MIN;
+    int n = arr.size();
+    for(int i=0; i<n; i++) {
+        if(prefix == 0) prefix = 1;
+        if(suffix == 0) suffix =1;
+        
+        prefix = prefix * arr[i];
+        suffix = suffix * arr[n-i-1];
+        ans = max(ans , max(prefix, suffix));
+    }
+    return ans;
+}
