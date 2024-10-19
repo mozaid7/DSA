b208b9c3f5edc80bc75b58a7787387ba51f717ac
Mohammed Zaid <mohammedzee98@gmail.com>
2024-10-19 19:56:55 +0530

    2 questions

--- binarySearch1.cpp
+++ binarySearch1.cpp
@@ -163,3 +163,61 @@ vector<int> searchRange(vector<int>& arr, int k) {
     if (lb == n || arr[lb] != k) return {-1, -1};
     return {lb, upperBound(arr, k)-1};
 }
+
+// Search in a Sorted Rotated array
+int search(vector<int>& arr, int n, int k){
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if(arr[mid] == k) return mid;
+
+        if(arr[low] <= arr[mid]) {
+            if(arr[low] <= k && k <= arr[mid]) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        else {
+            if(arr[mid] <= k && k <= arr[high]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+// Search in a Sorted Rotated array with Duplicates
+int search(vector<int>& arr, int n, int k){
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if(arr[mid] == k) return true;
+        if(arr[low] == arr[mid] && arr[mid] == arr[high]) {
+            low++, high--;
+            continue;
+        }
+
+        if(arr[low] <= arr[mid]) {
+            if(arr[low] <= k && k <= arr[mid]) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        else {
+            if(arr[mid] <= k && k <= arr[high]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+    }
+    return false;
+}
