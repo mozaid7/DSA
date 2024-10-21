87102db01a8ff7611a5cd20d1f78e392b490bb9a
Mohammed Zaid <mohammedzee98@gmail.com>
2024-10-21 16:13:27 +0530

    2 done

--- binarySearch1.cpp
+++ binarySearch1.cpp
@@ -221,3 +221,60 @@ int search(vector<int>& arr, int n, int k){
     }
     return false;
 }
+
+// Minimun from Rotated Sorted Array
+int findMin(vector<int>& arr) {
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    while(low <= high) {
+        int mid = (low + high) / 2;
+        if(arr[low] <= arr[mid]) {
+            ans = min(ans, arr[low]);
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+            ans = min(ans, arr[mid]);
+        }
+    }
+    return ans;
+}
+
+// No of times the array is rotated
+int findKRotation(vector<int> &arr) {
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    int index = -1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        //search space is already sorted.
+        //then arr[low] will always be the minimum in that search space:
+        if (arr[low] <= arr[high]) {
+            if (arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+            break;
+        }
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            // keep the minimum:
+            if (arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+            // Eliminate left half:
+            low = mid + 1;
+        }
+        else { //if right part is sorted:
+            // keep the minimum:
+            if (arr[mid] < ans) {
+                index = mid;
+                ans = arr[mid];
+            }
+            // Eliminate right half:
+            high = mid - 1;
+        }
+    }
+    return index;
+}
