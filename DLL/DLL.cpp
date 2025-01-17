53b04e297f8147a5f6187b5c01270dc90799b045
Mohammed Zaid <mohammedzee98@gmail.com>
2025-01-17 20:34:26 +0530

    DLL ques

--- DLL/DLL.cpp
+++ DLL/DLL.cpp
@@ -41,9 +41,28 @@ void print(Node* head){
     }
 }
 
+// int main(){
+//     vector<int> arr = {12,4,3,5};
+//     Node* head = convertArrToDLL(arr);
+//     print(head);
+//     return 0;
+// }
+
+Node* deleteHead(Node* head){
+    if(head == NULL || head->next == NULL){
+        return NULL;
+    }
+    Node* prev = head;
+    head = head->next;
+    head->back = nullptr;
+    prev->next = nullptr;
+    delete prev;
+    return head;
+}
 int main(){
-    vector<int> arr = {12,4,3,5};
+    vector<int> arr = {12,34,8,4,3,5};
     Node* head = convertArrToDLL(arr);
+    head = deleteHead(head);
     print(head);
     return 0;
 }
