119c6c036bb9f69e0c2172832a765588ec7bc3af
Mohammed Zaid <mohammedzee98@gmail.com>
2025-01-21 23:33:43 +0530

    start of loop

--- LL/LL.cpp
+++ LL/LL.cpp
@@ -348,3 +348,22 @@ bool detectLoop(Node* head){
     }
     return false;
 }
+
+// Detecting the starting node of the loop
+Node* startOfLoop(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            slow = head;
+            while(slow != fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
