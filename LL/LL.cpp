7f9f017841cea0811aab3f90d620dbc738365727
Mohammed Zaid <mohammedzee98@gmail.com>
2025-01-22 23:46:24 +0530

    ques detect

--- LL/LL.cpp
+++ LL/LL.cpp
@@ -369,3 +369,21 @@ Node* startOfLoop(Node* head){
 }
 
 // Fake commit created
+// Detecting the starting node of the loop
+Node* startOfLoop(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            slow = head;
+            while(slow != fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
