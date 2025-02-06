8560f425d6fcc93f4c600ad1d30709acb4abba58
Mohammed Zaid <mohammedzee98@gmail.com>
2025-02-06 23:57:33 +0530

    AddOne to LL

--- LL/LL.cpp
+++ LL/LL.cpp
@@ -550,3 +550,25 @@ Node* deleteMid(Node* head){
     delete(middle);
     return head;
 }
+
+// Add one to the Data of the LL and change the LL ex. 1->5->9 + 1 == 1->6->0
+int addHelper(Node* temp){
+    if(temp == NULL){
+        return 1;
+    }
+    int carry = addHelper(temp->next);
+    temp->data += carry;
+    if(temp->data < 10) return 0;
+    temp->data = 0;
+    return 1;
+
+}
+Node* addOne(Node* head){
+    int carry = addHelper(head);
+    if(carry == 1){
+        Node* newNode = new Node(1);
+        newNode->next = head;
+        return newNode;
+    }
+    return head;
+}
