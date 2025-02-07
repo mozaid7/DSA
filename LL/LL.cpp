889689d65a336b4bf70e4f0dece5f91ca32878d5
Mohammed Zaid <mohammedzee98@gmail.com>
2025-02-07 23:48:11 +0530

    addTwo to LL

--- LL/LL.cpp
+++ LL/LL.cpp
@@ -572,3 +572,28 @@ Node* addOne(Node* head){
     }
     return head;
 }
+
+// Add two numbers in a LL
+Node* addTwo(Node* head1, Node* head2){
+    Node* t1 = head1;
+    Node* t2 = head2;
+    Node* dummyNode = new Node(-1);
+    Node* curr = dummyNode;
+    int carry = 0;
+    while(t1 != NULL || t2 != NULL){
+        int sum = carry;
+        if (t1) sum = sum + t1->data;
+        if (t2) sum = sum + t2->data;
+        Node* newNode = new Node(sum % 10);
+        carry = sum/10;
+        curr->next = newNode;
+        curr = curr->next;
+        if(t1) t1 = t1->next;
+        if(t2) t2 = t2->next;
+    }
+    if(carry) {
+        Node* newNode = new Node(carry);
+        curr->next = newNode;
+    }
+    return dummyNode->next;
+}
