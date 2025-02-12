6e13d9c65c76d81f5fa042c936a02f54c77be2fa
Mohammed Zaid <mohammedzee98@gmail.com>
2025-02-12 22:01:26 +0530

    rotate

--- LL/LL.cpp
+++ LL/LL.cpp
@@ -601,4 +601,31 @@ Node* addTwo(Node* head1, Node* head2){
     return dummyNode->next;
 }
 
-// new code to be written hello
+// Rotate a LL by K times
+Node* findNthNode(Node* temp, int k){
+    int cnt = 1;
+    while(temp != NULL) {
+        if(cnt == k) return temp;
+        cnt++;
+        temp = temp->next;
+    }
+    return temp;
+}
+
+Node* rotate(Node* head, int k){
+    if(head == NULL || k == 0) return head;
+    Node* tail = head;
+    int len = 1;
+    while(tail->next != NULL){
+        tail = tail->next;
+        len += 1;
+    }
+    if(k % len == 0) return head;
+    k = k % len;
+
+    tail->next = head;
+    Node* newLastNode = findNthNode(head, len - k);
+    head = newLastNode->next;
+    newLastNode->next = NULL;
+    return head;
+}
