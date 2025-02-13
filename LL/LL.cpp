ff5dc3fad463e04f0259ff2675c5580e1c4bf4fb
Mohammed Zaid <mohammedzee98@gmail.com>
2025-02-13 22:06:31 +0530

    flatten a LL

--- LL/LL.cpp
+++ LL/LL.cpp
@@ -629,3 +629,35 @@ Node* rotate(Node* head, int k){
     newLastNode->next = NULL;
     return head;
 }
+
+// Flattening a LL
+Node* merge2Lists(Node* list1, Node* list2){
+    Node* dummy = new Node(-1);
+    Node* res = dummy;
+    while(list1 != NULL && list2 != NULL){
+        if(list1->data < list2->data){
+            // res->child = list1;
+            res = list1;
+            // list1 = list1->child;
+        }
+        else {
+            // res->child = list2;
+            res = list2;
+            // list2 = list2->child;
+        }
+        res->next = NULL;
+    }
+    // if(list1) res->child = list1;
+    // else res->child = list2;
+    // if(dummy->child) dummy->child->next = NULL;
+    // return dummy->child;
+}
+
+Node* flatten(Node* head){
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+    Node* mergeHead = flatten(head->next);
+    head =  merge2Lists(head, mergeHead);
+    return head;
+}
