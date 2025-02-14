a90f53a64d42dbd68e82207f3540644028b3bb68
Mohammed Zaid <mohammedzee98@gmail.com>
2025-02-15 00:28:23 +0530

    var changed

--- LL/LL.cpp
+++ LL/LL.cpp
@@ -667,10 +667,10 @@ Node* reverseLinkedList(Node *head){
    Node* temp = head;  
    Node* prev = NULL;  
    while(temp != NULL){  
-       Node* front = temp->next;  
+       Node* current = temp->next;  
        temp->next = prev;  
        prev = temp;  
-       temp = front; 
+       temp = current; 
    }
    return prev;  
 }
