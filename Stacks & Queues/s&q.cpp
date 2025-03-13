197ef18343d5f025774fb9231a290301f2f4df74
Mohammed Zaid <mohammedzee98@gmail.com>
2025-03-14 03:23:24 +0530

    sliding window

--- Stacks & Queues/s&q.cpp
+++ Stacks & Queues/s&q.cpp
@@ -192,3 +192,21 @@ list<int> asteroid(vector<int> &arr){
     }
     return st;
 };
+
+//Sliding Window Maximum
+vector<int> maxWindow(vector<int> &arr, int k){
+    int n = arr.size();
+    vector<int> lst;
+    deque<int> dq;
+    for(int i = 0; i<n; i++){
+        if(!dq.empty() && dq.front() <= i-k){
+            dq.pop_front();
+        }
+        while(!dq.empty() && arr[dq.back()] <= arr[i]){
+            dq.pop_back();
+        }
+        dq.push_back(i);
+        if(i >= k-1) lst.push_back(arr[dq.front()]);
+    }
+    return lst;
+}
