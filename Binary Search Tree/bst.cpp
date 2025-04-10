0c83013a4ee533f9f3f2839c89465e082f5f21db
Mohammed Zaid <mohammedzee98@gmail.com>
2025-04-11 01:32:00 +0530

    deleteBST

--- Binary Search Tree/bst.cpp
+++ Binary Search Tree/bst.cpp
@@ -41,3 +41,45 @@ Node* insertBST(Node* root, int val){
     }
     return root;
 }
+
+// Delete in a BST
+Node* deleteBST(Node* root, int key){
+    if(root == NULL) return NULL;
+    if(root->data == key) return helper(root);
+
+    Node* dummy = root;
+    while(root != NULL){
+        if(root->data > key){
+            if(root->left != NULL && root->left->data == key){
+                root->left = helper(root->left);
+                break;
+            } else {
+                root = root->left;
+            }
+        } else {
+            if(root->right != NULL && root->right->data == key){
+                root->right = helper(root->right);
+                break;
+            } else {
+                root = root->right;
+            }
+        }
+    }
+    return dummy;
+}
+Node* helper(Node* root){
+    if(root->left == NULL){
+        return root->right;
+    } 
+    else if(root->right == NULL){
+        return root->left;
+    }
+    Node* rightChild = root->right;
+    Node* lastRight = findLastRight(root->left);
+    lastRight->right = rightChild;
+    return root->left;
+}
+Node* findLastRight(Node* root){
+    if(root == NULL) return root;
+    return findLastRight(root->right);
+}
