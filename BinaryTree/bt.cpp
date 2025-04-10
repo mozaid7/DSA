e908c864bdc3ba371a89d641dfd3416512fc2836
Mohammed Zaid <mohammedzee98@gmail.com>
2025-04-11 00:16:39 +0530

    levelOrder

--- BinaryTree/bt.cpp
+++ BinaryTree/bt.cpp
@@ -37,4 +37,23 @@ int maxDepth(Node* root){
     return 1 + max(lh, rh);
 }
 
-// Level Order Traversal todo
+// Level Order Traversal
+vector<vector<int>> levelOrder(Node* root){
+    vector<vector<int>> ans;
+    if(root == NULL) return ans;
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty()){
+        int size = q.size();
+        vector<int> level;
+        for(int i = 0; i<size; i++){
+            Node* node = q.front();
+            q.pop();
+            if(node->left != NULL) q.push(node->left);
+            if(node->right != NULL) q.push(node->right);
+            level.push_back(node->data);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
