356fe6fb74b181d51984ccbefea988ae7c4c9b54
Mohammed Zaid <mohammedzee98@gmail.com>
2025-04-18 09:19:02 +0530

    in class ques.

--- CRT/dsa-2.cpp
+++ CRT/dsa-2.cpp
@@ -20,3 +20,69 @@ vector<int> plusOne(vector<int>& digits) {
     }
     return digits;
 }
+
+// Largest Number
+string largestNumber(vector<int>& nums) {
+    vector<string> arr;
+    for(int num : nums){
+        arr.push_back(to_string(num));
+    }
+
+    // VERY IMP method. revise again !!
+    sort(arr.begin(), arr.end(), [](const string &a, const string &b){
+        return a+b > b+a;
+    });
+
+    if(arr[0] == "0"){
+        return "0";
+    }
+
+    string largest;
+    for(const string &num : arr){
+        largest += num;
+    }
+    return largest;
+}
+
+// Most Frequent Even Element
+int mostFrequentEven(vector<int>& nums) {
+    map<int, int>mpp;
+    for(int num : nums){
+        if(num%2 == 0){
+            mpp[num] += 1;
+        }  
+    }
+
+    map<int, int>::iterator it;
+    int maxFreqNumber = -1;
+    int maxFreq = 0;
+    for(it = mpp.begin(); it != mpp.end(); it++){
+        if(it->second > maxFreq){
+            maxFreq = it->second;
+            maxFreqNumber = it->first;
+        } else if(it->second == maxFreq){
+            maxFreqNumber = min(maxFreqNumber, it->first);
+        }
+    }
+
+    return maxFreqNumber;
+}
+
+// Simplified and Compact Version
+int mostFrequentEven(vector<int>& nums) {
+    unordered_map<int, int> freq;
+    int maxFreq = 0, result = -1;
+
+    for (int num : nums) {
+        if (num % 2 == 0) {
+            freq[num]++;
+            if (freq[num] > maxFreq || 
+               (freq[num] == maxFreq && num < result)) {
+                maxFreq = freq[num];
+                result = num;
+            }
+        }
+    }
+
+    return result;
+}
