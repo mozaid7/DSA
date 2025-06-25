a954b1b676723a2595a8892ed8c6014978dd9592
Mohammed Zaid <mohammedzee98@gmail.com>
2025-06-25 22:30:59 +0530

    ques

--- CRT/AB.cpp
+++ CRT/AB.cpp
@@ -902,3 +902,30 @@ public:
         m[key] = head -> next;
     }
 };
+
+// Find Median in Data Stream with Priority Queue
+class MedianFinder {
+public:
+    priority_queue<int> left;
+    priority_queue<int, vector<int>, greater<int>> right;
+
+    void addNum(int num) {
+        left.push(num);
+
+        right.push(left.top());
+        left.pop();
+
+        if (right.size() > left.size()) {
+            left.push(right.top());
+            right.pop();
+        }
+    }
+
+    double findMedian() {
+        if (left.size() == right.size()) {
+            return (left.top() + right.top()) / 2.0;
+        } else {
+            return left.top();
+        }
+    }
+};
