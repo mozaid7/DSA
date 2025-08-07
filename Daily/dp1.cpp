ce188629f6a7bfdb5cd465fcca494658d5c0d31c
Mohammed Zaid <mohammedzee98@gmail.com>
2025-08-07 23:31:29 +0530

    dpp2

--- Daily/dp1.cpp
+++ Daily/dp1.cpp
@@ -69,3 +69,30 @@ vector<int> frequencySort(vector<int>& nums) {
 
     return nums;
 }
+
+// Hand Of Straights
+bool isNStraightHand(vector<int>& hand, int groupSize) {
+    int n = hand.size();
+    if(n % groupSize != 0) {
+        return false;
+    }
+
+    map<int,int> mp;
+    for(int &handNum : hand){
+        mp[handNum]++;
+    }
+
+    while(!mp.empty()){
+        int curr = mp.begin()->first;
+        for(int i=0; i<groupSize; i++){
+            if(mp[curr + i] == 0){
+                return false;
+            }
+            mp[curr + i]--;
+            if(mp[curr + i] == 0){
+                mp.erase(curr + i);
+            }
+        }
+    }
+    return true;
+}
