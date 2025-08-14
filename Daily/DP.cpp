345e3cb35eeec600b09b357df5f59303c7a847ef
Mohammed Zaid <mohammedzee98@gmail.com>
2025-08-15 00:38:09 +0530

    dp1

--- Daily/DP.cpp
+++ Daily/DP.cpp
@@ -36,5 +36,65 @@ int rob(vector<int>& nums) {
         dp[i] = max(dp[i-2]+nums[i], dp[i-1]);
     }
     return dp[n-1];
+}
 
+// 213. House Robber 2
+int rob(vector<int>& nums) {
+    int n = nums.size();
+    if(n < 2) return nums[0];
+    
+    vector<int> skipLast(n-1);
+    vector<int> skipFirst(n-1);
+
+    for(int i=0; i<n-1; i++){
+        skipLast[i] = nums[i];
+        skipFirst[i] = nums[i+1];
+    }
+
+    int lootSkipLast = robHelp(skipLast);
+    int lootSkipFirst = robHelp(skipFirst);
+
+    return max(lootSkipLast, lootSkipFirst);
+}
+
+int robHelp(vector<int>& nums) {
+    int n = nums.size();
+    if(n < 2) return nums[0];
+
+    int dp[n];
+    dp[0] = nums[0];
+    dp[1] = max(nums[0], nums[1]);
+
+    for(int i=2; i<n; i++){
+        dp[i] = max(dp[i-2]+nums[i], dp[i-1]);
+    }
+    return dp[n-1];
+}
+
+// Coin Change - 2 
+int dp[1001][10001];
+int countWays(vector<int>& coins, int amount, int idx){
+    if (idx == 0) {
+        if (amount % coins[0] == 0) 
+            return amount / coins[0];
+        else 
+            return 1e9;
+    }
+
+    if(dp[idx][amount] != -1) return dp[idx][amount];
+
+    int notTake = countWays(coins, amount, idx-1);
+    int take = 1e9;
+    if(coins[idx] <= amount){
+        take = 1 + countWays(coins, amount-coins[idx], idx);
+    }
+
+    return dp[idx][amount] = min(take, notTake);
+}
+
+int coinChange(vector<int>& coins, int amount) {
+    int n = coins.size();
+    memset(dp, -1, sizeof(dp));
+    int ans = countWays(coins, amount, n - 1);
+    return (ans >= 1e9) ? -1 : ans;
 }
