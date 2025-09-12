d5292b21d53843e239a1c287b47e65432f4a6fa2
Mohammed Zaid <mohammedzee98@gmail.com>
2025-09-12 21:49:19 +0530

    dy pr

--- Daily/DP.cpp
+++ Daily/DP.cpp
@@ -98,3 +98,46 @@ int coinChange(vector<int>& coins, int amount) {
     int ans = countWays(coins, amount, n - 1);
     return (ans >= 1e9) ? -1 : ans;
 }
+
+// Longest Common Subsequence
+int longestCommonSubsequence(string text1, string text2) {
+    int n = text1.length();
+    int m = text2.length();
+
+    vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(text1[i-1] == text2[j-1]){
+                dp[i][j] = dp[i-1][j-1]+1;
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+    return dp[n][m];
+}
+
+// Edit Distance
+int minDistance(string word1, string word2) {
+    int n = word1.length();
+    int m = word2.length();
+    vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+
+    for(int i=1; i<=n; i++){
+        dp[i][0] = i;
+    }
+    for(int j=1; j<=m; j++){
+        dp[0][j] = j;
+    }
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(word1[i-1] == word2[j-1]){
+                dp[i][j] = dp[i-1][j-1];
+            } else {
+                dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1;
+            }
+        }
+    }
+    return dp[n][m];
+}
